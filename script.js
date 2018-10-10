0950a7b2928743a7b4a2d8891cd4f1350fb9a348
Morshedul Alam Murad <morshedulalam80@gmail.com>
2018-10-10 03:32:36 +0000

    Task 2b

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 /* Task 1 */
+var heading =document.getElementById('heading')
+console.log(heading.innerText);
 
 
 /* Task 2 using plain JavaScript */
 
-
+var textBtn = document.getElementById('addText2');
+textBtn.addEventListener('click',function(event){
+    var task2=document.getElementById('task2a');
+   var paragraph=document.createElement('p');
+   
+    paragraph.innerText="Hello World";
+    task2.appendChild(paragraph);
+    
+});
 
 /* Task 4 using jQuery */
